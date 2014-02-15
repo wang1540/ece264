735e269ccbf0f9ac2c8ee57b6b3bbc68c2535e37
Xihui Wang <wang1540@ecelinux07.ecn.purdue.edu>
2014-02-14 23:35:47 -0500

    grep-lite.c

--- PA04/grep-lite.c
+++ PA04/grep-lite.c
@@ -30,10 +30,11 @@ int main (int argc, char * * argv)
   for (i = 1; i < argc-1; i++)    {
     if (strcmp (argv[i],"--help") == 0)    showHelp = TRUE;
     else if (strcmp (argv[i],"--invert-match") == 0)    invertMatch = TRUE;
-    else if (strcmp (argv[i],"--v") == 0)    invertMatch = TRUE;
-    else if (strcmp (argv[i],"--n") == 0)    lineNumbers = TRUE;
+    else if (strcmp (argv[i],"-v") == 0)    invertMatch = TRUE;
+    else if (strcmp (argv[i],"-n") == 0)    lineNumbers = TRUE;
     else if (strcmp (argv[i],"--line-number") == 0)    lineNumbers = TRUE;
     else if (strcmp (argv[i],"--quiet") == 0)    quiet = TRUE;
+    else if (strcmp (argv[i],"-q") == 0)    quiet = TRUE;
     else   {
       fprintf(stderr,"error!\n");
       return ERROR;
@@ -42,7 +43,7 @@ int main (int argc, char * * argv)
   }
   
 
-  if (showHelp || strcmp (pattern, "help") == 0)    {
+  if (showHelp || strcmp (pattern, "--help") == 0)    {
     printHelp();
     return EXIT_SUCCESS;
   }
@@ -57,21 +58,19 @@ int main (int argc, char * * argv)
     currLine++;
     int matches = strstr(buffer, pattern) != NULL;
     if ((matches && !invertMatch) || (!matches && invertMatch))    {
-    found = TRUE;
-    if (!quiet)    {
-      if(lineNumbers)    {
-	  printf("%d", currLine);
-	  printf("%s", buffer);
-        }
+      found = TRUE;
+      if (!quiet)    {
+	if(lineNumbers)    {
+	  printf("%d:", currLine);
+	}
+	printf("%s", buffer);
       }
     }
-    return found ? 0 : 1;
   }
+  
 
+  return found ? 0 : 1;
 
-
-
-  return EXIT_SUCCESS;
 }
 
 
@@ -85,11 +84,10 @@ void printHelp()
   printf("line, and (by default) print out those lines which\n");
   printf("contain pattern as a substring.\n");
   printf("\n");
-  printf("  -v, --invert-match     print non-matching lines\n");
+  printf("  -v, --invert-match     print non-matching lines");
   printf("  -n, --line-number      print line number with output\n");
   printf("  -q, --quiet            suppress all output\n");
   printf("\n");
   printf("Exit status is 0 if any line is selected, 1 otherwise;\n");
   printf("if any error occurs, then the exit status is 2.\n");
-  printf("\n");
 }
