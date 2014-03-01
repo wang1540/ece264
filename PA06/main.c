bf597f4c68e8d6b7366d245b6ce03dbf5ecd4532
Xihui Wang <wang1540@ecelinux21.ecn.purdue.edu>
2014-02-28 21:26:07 -0500

    main.c

---
+++ PA06/main.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+
+#include "answer06.h"
+
+
+int main (int argc, char ** argv)
+{
+  //ask teacher about this part!!!!!!!!!!!!!!!!!!!!!!!!
+  
+  //if(argc != 2)    return EXIT_FAILURE;
+  //int val = atoi(argv[2]);
+  int val = 9;
+  //partitionAll (val);
+  //partitionIncreasing (val);
+  //partitionDecreasing (val);
+  //partitionOdd (val);
+  //partitionEven (val);
+  partitionOddAndEven (val);
+  //printf("%d\n",4 % 2);
+  //partitionPrime (val);
+  return EXIT_SUCCESS;
+
+}
